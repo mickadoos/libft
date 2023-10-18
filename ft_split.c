842e1a6a1cc54d194e48a6df452c5858a72fa383
Yabel <yabelrr@gmail.com>
2023-10-18 16:51:43 +0200

    Split finished. Working on programs to test

--- ft_split.c
+++ ft_split.c
@@ -25,33 +25,58 @@ static	size_t	countwords(const char *s, char c)
 		while (s[i] == c)
 			i++;
 		while (s[i] && s[i] != c)
+		{
 			i++;
-		if (s[i])
-			words++;
+			if (s[i] == c || !s[i])
+				words++;
+		}
 	}
 	return (words);
 }
 
-static	char	**getwords(const char *s, char c, size_t numwords)
+static void	*ft_memoryfree(char **str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+	{
+		free(str[i]);
+		i++;
+	}
+	free(str);
+	return (NULL);
+}
+
+static	unsigned int	skipseparators(const char *s, char c, size_t *index)
+{
+	while (s[*index] == c)
+		*index += 1;
+	return (*index);
+}
+
+static	char	**getwords(const char *s, char c, char	**str_arr)
 {
 	size_t			i;
 	size_t			j;
 	unsigned int	start;
-	char			**str_arr;
 
 	i = 0;
 	j = 0;
 	start = 0;
-	str_arr = (char **)malloc(sizeof(char *) * numwords + 1);
 	while (s[i])
 	{
-		while (s[i] == c)
-			i++;
-		start = i;
+		start = skipseparators(s, c, &i);
 		while (s[i] && s[i] != c)
+		{
 			i++;
-		if (s[i])
-			str_arr[j++] = ft_substr(s, start, (i - start));
+			if (s[i] == c || !s[i])
+			{
+				str_arr[j] = ft_substr(s, start, (i - start));
+				if (!str_arr[j++])
+					return (ft_memoryfree(str_arr));
+			}
+		}
 	}
 	str_arr[j] = NULL;
 	return (str_arr);
@@ -63,6 +88,9 @@ char	**ft_split(const char *s, char c)
 	size_t	numwords;
 
 	numwords = countwords(s, c);
-	str_arr = getwords(s, c, numwords);
+	str_arr = (char **)malloc(sizeof(char *) * (numwords + 1));
+	if (!str_arr)
+		return (NULL);
+	str_arr = getwords(s, c, str_arr);
 	return (str_arr); 
 }
